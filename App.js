ce89ed88e013b1b1a8c69d3fd36cdf4a2e380aaa
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-05-26 16:31:05 +0600

    font-setup-done

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { colors } from "./src/theme/colors";
+import { spacing } from "./src/theme/spacing";
+import {useFonts} from "expo-font"
 
 export default function App() {
+  const [loaded] = useFonts({
+    'Antonio-Medium': require('./assets/fonts/Antonio-Medium.ttf'),
+    'Spartan-Bold': require('./assets/fonts/LeagueSpartan-Bold.ttf'),
+    'Spartan-Regular': require('./assets/fonts/LeagueSpartan-Regular.ttf')
+  })
+
+  if (!loaded) {
+    return <Text>Font is loading</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>This is planet app</Text>
+      <Text style={{fontFamily: 'Antonio-Medium', fontSize: spacing[4]}}>This is planet app</Text>
+      <Text style={{marginTop: spacing[4]}}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.orange,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
