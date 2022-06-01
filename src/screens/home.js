0c597068f1311bab21158d8f1b419740d8e7cada
Rakibul-Hasan-max <rakibulhasan4041@gmail.com>
2022-06-01 21:06:45 +0600

    home page

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,38 @@
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { FlatList, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import PlanetHeader from "../components/planet-header";
 import Text from "../components/text/text";
+import { PLANET_LIST } from "../data/planet-list";
 import { colors } from "../theme/colors";
+import { spacing } from "../theme/spacing";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function Home() {
   return (
     <SafeAreaView style={styles.container}>
       <PlanetHeader />
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={PLANET_LIST}
+        keyExtractor={(item) => item.name}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.item}>
+              <View style={{ flexDirection: "row", alignItems: "center" }}>
+                <View
+                  style={[styles.circle, { backgroundColor: item.color }]}
+                />
+                <Text preset="h4" style={styles.itemName}>
+                  {item.name}{" "}
+                </Text>
+              </View>
+              <AntDesign name="right" size={14} color="white" />
+            </View>
+          );
+        }}
+        ItemSeparatorComponent={() => <View style={styles.separator} />}
+      />
     </SafeAreaView>
   );
 }
@@ -17,5 +41,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.black,
+  },
+  item: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    padding: spacing[4],
+  },
+  itemName: {
+    textTransform: "uppercase",
+    marginLeft: spacing[4],
+  },
+  circle: {
+    height: 26,
+    width: 26,
+    borderRadius: 13,
+  },
+  list: {
+    padding: spacing[4],
+  },
+  separator: {
+    borderWidth: 0.3,
+    borderBottomColor: colors.grey,
   },
 });
